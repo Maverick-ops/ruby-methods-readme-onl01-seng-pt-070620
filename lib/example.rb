0a2fa336707c77bf01c6b3876c5bffe230091858
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-08 00:39:04 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -11,17 +11,4 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-def say_hello_world_ten_times
-  phrase = "Hello World"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
-say_hello_world_ten_times
+
